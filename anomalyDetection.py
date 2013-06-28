e5ec4e41d5c7f618de3b33ea40d3411d88f71ecb
emilydolson <EmilyLDolson@gmail.com>
2013-06-28 12:51:24 -0400

    fix -r flag

--- anomalyDetection.py
+++ anomalyDetection.py
@@ -288,7 +288,7 @@ def main():
     parser.add_option("-s", "--historySize", default=2, dest="historySize", type= "int", help="History size for the RAVQ.")
     parser.add_option("-l", "--learningRate", action="store", default=.2, nargs=1, dest="learningRate", type= "float", help="Learning rate for the ARAVQ")
     parser.add_option("-f", "--sensorFiles", action="store",default=["wxsta1_alldat.csv"], dest="sensorFiles", help="Files containing data to used.")
-    parser.add_option("-r", "--removeStreams", action = "store", default=[], dest="removeStreams", help="Streams to not use.", type="string")
+    parser.add_option("-r", "--removeStreams", action = "store", default="[]", dest="removeStreams", help="Streams to not use.", type="string")
     parser.add_option("-c", "--checkid", action = "store", default="", dest="checkid", help="ID of checkpoint to save files under.")
     (opts, args) = parser.parse_args()
     print sys.argv
